b1b83b61a1ae7220d8063df427c19a2e459d1979
William Jagels <will.jagels@gmail.com>
2016-11-26 08:14:42 +0200

    Fix up poorly formatted code

--- qtapp/imagelabel.hpp
+++ qtapp/imagelabel.hpp
@@ -2,58 +2,46 @@
 #ifndef IMAGELABEL_HPP
 #define IMAGELABEL_HPP
 
-class ImageLabel2 : public QWidget
-{
-    Q_OBJECT
+class ImageLabel2 : public QWidget {
+  Q_OBJECT
 
-public:
-    explicit ImageLabel2(QWidget *parent = 0);
-    const QPixmap* pixmap() const;
+ public:
+  explicit ImageLabel2(QWidget *parent = 0);
+  const QPixmap *pixmap() const;
 
-public slots:
-    void setPixmap(const QPixmap&);
+ public slots:
+  void setPixmap(const QPixmap &);
 
-protected:
-    void paintEvent(QPaintEvent *);
+ protected:
+  void paintEvent(QPaintEvent *);
 
-private:
-    QPixmap pix;
+ private:
+  QPixmap pix;
 };
 
 // imagelabel2.cpp
 
-ImageLabel2::ImageLabel2(QWidget *parent) :
-    QWidget(parent)
-{
-}
+ImageLabel2::ImageLabel2(QWidget *parent) : QWidget(parent) {}
 
 void ImageLabel2::paintEvent(QPaintEvent *event) {
-    QWidget::paintEvent(event);
+  QWidget::paintEvent(event);
 
-    if (pix.isNull())
-        return;
+  if (pix.isNull()) return;
 
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
+  QPainter painter(this);
+  painter.setRenderHint(QPainter::Antialiasing);
 
-    QSize pixSize = pix.size();
-    pixSize.scale(event->rect().size(), Qt::KeepAspectRatio);
+  QSize pixSize = pix.size();
+  pixSize.scale(event->rect().size(), Qt::KeepAspectRatio);
 
-    QPixmap scaledPix = pix.scaled(pixSize,
-                                   Qt::KeepAspectRatio,
-                                   Qt::SmoothTransformation
-                                   );
+  QPixmap scaledPix =
+      pix.scaled(pixSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
-    painter.drawPixmap(QPoint(), scaledPix);
-
+  painter.drawPixmap(QPoint(), scaledPix);
 }
 
-const QPixmap* ImageLabel2::pixmap() const {
-    return &pix;
-}
+const QPixmap *ImageLabel2::pixmap() const { return &pix; }
 
-void ImageLabel2::setPixmap (const QPixmap &pixmap){
-    pix = pixmap;
-}
+void ImageLabel2::setPixmap(const QPixmap &pixmap) { pix = pixmap; }
 
-#endif // IMAGELABEL_HPP
+#endif  // IMAGELABEL_HPP
--- qtapp/mainwindow.cpp
+++ qtapp/mainwindow.cpp
@@ -1,41 +1,35 @@
-#include <iostream>
-#include <cstdlib>
+#include "mainwindow.h"
 #include <QFileDialog>
+#include <cstdlib>
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include "conversion.hpp"
 #include "expandpath.hpp"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_pushButton_2_clicked() {
+  QString fileName = QFileDialog::getOpenFileName(
+      this, tr("Open Image"), QDir::home().absolutePath(), tr("*.png *.jpg"),
+      Q_NULLPTR, QFileDialog::Options(QFileDialog::DontUseNativeDialog));
+  if (fileName.isNull()) return;
+  cvimage = cv::imread(fileName.toStdString(), CV_LOAD_IMAGE_COLOR);
+  cv::Mat greyed;
+  cv::cvtColor(cvimage, greyed, CV_BGR2GRAY);
+  QImage qim1 = Mat2QImage(cvimage);
+  auto picbox = ui->picbox;
+  picbox->setScaledContents(true);
+  picbox->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+  picbox->setPixmap(QPixmap::fromImage(qim1));
 }
 
-void MainWindow::on_pushButton_2_clicked()
-{
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), QDir::home().absolutePath(), tr("*.png *.jpg"), Q_NULLPTR, QFileDialog::Options(QFileDialog::DontUseNativeDialog));
-    if (fileName.isNull()) return;
-    cvimage = cv::imread(fileName.toStdString(), CV_LOAD_IMAGE_COLOR);
-    cv::Mat greyed;
-    cv::cvtColor(cvimage, greyed, CV_BGR2GRAY);
-    QImage qim1 = Mat2QImage(cvimage);
-    auto picbox = ui->picbox;
-    picbox->setScaledContents(true);
-    picbox->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
-    picbox->setPixmap(QPixmap::fromImage(qim1));
-}
-
-void MainWindow::on_pushButton_clicked()
-{
-   if (cvimage.empty())
-       return;
+void MainWindow::on_pushButton_clicked() {
+  if (cvimage.empty()) return;
 }
